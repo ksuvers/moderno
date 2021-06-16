06b6c5756a2dcd8b1f635fdaa9c4ec88ff13b115
ksuvers <ksulivigo@gmail.com>
2021-06-16 13:31:30 +0300

    added product, product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function() {
-
-
-
-
+    /* для подключения mixitup */
+    var mixer = mixitup('.products__inner-box');
+    /* для подключения звезд в отзывах */
+    $(".rate-star").rateYo({
+        starWidth: "12px",
+        rating: 5,
+        readOnly: true
+    });
+    /* для slider */
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
