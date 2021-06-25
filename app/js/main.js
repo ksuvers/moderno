9d7c7e2f479d7bc0bf91be1b11c4a638b70d52df
ksuvers <ksulivigo@gmail.com>
2021-06-25 13:36:42 +0300

    adaptation header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function() {
-    /* для подключения mixitup */
-    var mixer = mixitup('.products__inner-box');
+
     /* для подключения звезд в отзывах */
     $(".rate-star").rateYo({
         starWidth: "12px",
@@ -14,4 +13,16 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.header__box').toggleClass('active');
+    });
+
+    /* для подключения mixitup */
+    var mixer = mixitup('.products__inner-box');
+
 });
