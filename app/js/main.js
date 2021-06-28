309c2c918ddbb1561f695d5e3e417922991a47ed
ksuvers <ksulivigo@gmail.com>
2021-06-28 18:07:50 +0300

    added new page

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,20 @@ $(function() {
     $('.header__btn-menu').on('click', function() {
         $('.header__box').toggleClass('active');
     });
+    /* для переключения на странице new-items */
+    $(".icon-th-list").on("click", function() {
+        $(".product__item").addClass("list");
+        $(".icon-th-list").addClass("active");
+        $(".icon-th-large").removeClass("active");
+    });
+    $(".icon-th-large").on("click", function() {
+        $(".product__item").removeClass("list");
+        $(".icon-th-large").addClass("active");
+        $(".icon-th-list").removeClass("active");
+    });
+
+
+
 
     /* для подключения mixitup */
     var mixer = mixitup('.products__inner-box');
