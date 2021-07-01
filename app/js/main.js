bdeafa2c570028331a465ec30bac30547c9bf8f7
ksuvers <ksulivigo@gmail.com>
2021-07-01 09:19:55 +0300

    added product-one

--- app/js/main.js
+++ app/js/main.js
@@ -34,7 +34,24 @@ $(function() {
     });
 
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$",
+    });
 
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
     /* для подключения mixitup */
     var mixer = mixitup('.products__inner-box');
